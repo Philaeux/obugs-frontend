bcc45b512de7139b264c21ff2c0845eced05e062
Vincent 'Philaeux' Lamotte <philaeux@the-cluster.org>
2022-05-12 13:56:14 +0200

    Move material elements to their module

--- obugs/src/app/app.module.ts
+++ obugs/src/app/app.module.ts
@@ -1,66 +1,46 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { IndexComponent } from './components/pages/index/index.component';
-import { SoftwareListComponent } from './components/pages/software-list/software-list.component';
-import { BugDetailsComponent } from './components/pages/bug-details/bug-details.component';
-import { BugSearchComponent } from './components/pages/bug-search/bug-search.component';
-import { LoginComponent } from './components/pages/login/login.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTreeModule } from '@angular/material/tree';
+
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
-import { AccountComponent } from './components/pages/account/account.component';
 
+import { MaterialModule } from './material/material.module';
+
+import { AccountComponent } from './components/pages/account/account.component';
+import { BugDetailsComponent } from './components/pages/bug-details/bug-details.component';
+import { BugSearchComponent } from './components/pages/bug-search/bug-search.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { IndexComponent } from './components/pages/index/index.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { SoftwareListComponent } from './components/pages/software-list/software-list.component';
 
 @NgModule({
   declarations: [
+    AccountComponent,
     AppComponent,
-    DashboardComponent,
-    IndexComponent,
-    SoftwareListComponent,
     BugDetailsComponent,
     BugSearchComponent,
+    DashboardComponent,
+    IndexComponent,
     LoginComponent,
-    AccountComponent,
+    SoftwareListComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatTreeModule,
+    MaterialModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth())
   ],
---
+++ obugs/src/app/material/material.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTreeModule } from '@angular/material/tree';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule,
+  MatIconModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatSidenavModule,
+  MatSortModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatTreeModule
+]
+
+@NgModule({
+  imports: [
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+
+export class MaterialModule { }
