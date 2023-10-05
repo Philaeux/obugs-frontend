27aaf07690cd7e126ecbef4b05e77104c7ebb537
Vincent 'Philaeux' Lamotte <vinc.lamotte@gmail.com>
2023-10-05 19:48:38 +0200

    Remove /

--- obugs/src/environments/environment.prod.ts
+++ obugs/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  obugsFrontend: 'https://obugs.the-cluster.org/',
-  obugsBackend: 'https://obugsapi.the-cluster.org/',
+  obugsFrontend: 'https://obugs.the-cluster.org',
+  obugsBackend: 'https://obugsapi.the-cluster.org',
   recaptchaSiteKey: '6Lf00GEoAAAAAF8EnFTENFbUG1htYMRVr9y_9q_R'
 };
