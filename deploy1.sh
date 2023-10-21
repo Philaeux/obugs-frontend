788db476a44dfdc633c00368950cff909599caf9
Vincent 'Philaeux' Lamotte <vinc.lamotte@gmail.com>
2023-10-22 00:29:12 +0200

    Fix readme

--- deploy1.sh
+++ deploy1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd obugs 
-npm install
 ng build --configuration production
 tar -czvf dist.tar.gz dist/
 scp dist.tar.gz philaeux@luna.the-cluster.org:~/obugs-frontend/obugs/dist.tar.gz
